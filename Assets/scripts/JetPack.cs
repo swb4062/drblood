769b78851c560471adbf155d82a453faad86678d
swb4062 <swb4062@louisiana.edu>
2014-11-16 20:11:54 -0600

    Jet Pack
    Made Jet Pack script and PreFab

---
+++ Assets/scripts/JetPack.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JetPack : MonoBehaviour {
+
+	private float gravity = 20.0f;
+	private Vector3 moveDirection = Vector3.zero;
+	private SpriteRenderer spriteRenderer;
+	public Sprite jetPackSprite;
+
+	Collision2D coll;
+
+	public PlatformerCharacter2D character;
+	// Use this for initialization
+	void OnCollisionEnter2D(Collision2D coll) {
+
+				if (coll.gameObject.tag == "Player") {
+
+						GameObject.Destroy (gameObject);
+	
+						character.rigidbody2D.gravityScale = 1;
+						moveDirection.y -= gravity * Time.deltaTime;
+						character.airControl = true;
+			
+						spriteRenderer = character.GetComponent<SpriteRenderer> ();
+						spriteRenderer.sprite = jetPackSprite;
+
+				}
+	}
+
+}
