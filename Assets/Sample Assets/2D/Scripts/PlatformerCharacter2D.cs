eb65256a934fa8cbc71afff445758611239c8d17
patrickrcharles <patrickrcharles@gmail.com>
2014-11-19 13:14:00 -0600

    RESIZE

--- Assets/Sample Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Sample Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -3,14 +3,14 @@
 public class PlatformerCharacter2D : MonoBehaviour 
 {
 	bool facingRight = true;							// For determining which way the player is currently facing.
-
+	
 	[SerializeField] float maxSpeed = 10f;				// The fastest the player can travel in the x axis.
-	[SerializeField] float jumpForce = 400f;			// Amount of force added when the player jumps.	
-
+	public float jumpForce = 400f;						// Amount of force added when the player jumps.	
+	
 	[Range(0, 1)]
 	[SerializeField] float crouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
 	
-	[SerializeField] bool airControl = false;			// Whether or not a player can steer while jumping;
+	public bool airControl = false;						// Whether or not a player can steer while jumping;
 	[SerializeField] LayerMask whatIsGround;			// A mask determining what is ground to the character
 	
 	Transform groundCheck;								// A position marking where to check if the player is grounded.
@@ -19,32 +19,42 @@ public class PlatformerCharacter2D : MonoBehaviour
 	Transform ceilingCheck;								// A position marking where to check for ceilings
 	float ceilingRadius = .01f;							// Radius of the overlap circle to determine if the player can stand up
 	Animator anim;										// Reference to the player's animator component.
-
-
-    void Awake()
+	Transform playerGraphics;							// Reference the player's graphics
+	public bool isAlive = true;							// Bool for to check if the player is alive. Used to trigger death animation
+	
+	public bool hasJetPack = false;						// Checks for a jetpack
+	
+	
+	void Awake()
 	{
 		// Setting up references.
 		groundCheck = transform.Find("GroundCheck");
 		ceilingCheck = transform.Find("CeilingCheck");
 		anim = GetComponent<Animator>();
+		
+		
 	}
-
-
-	void FixedUpdate()
+	
+	
+	void Update()
 	{
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
 		anim.SetBool("Ground", grounded);
-
+		
 		// Set the vertical animation
 		anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+		
+		anim.SetBool("hasJetPack", hasJetPack);
+		
+		anim.SetBool("isAlive", isAlive);
+		
 	}
-
-
+	
 	public void Move(float move, bool crouch, bool jump)
 	{
-
-
+		
+		
 		// If crouching, check to see if the character can stand up
 		if(!crouch && anim.GetBool("Crouch"))
 		{
@@ -52,19 +62,19 @@ public class PlatformerCharacter2D : MonoBehaviour
 			if( Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround))
 				crouch = true;
 		}
-
+		
 		// Set whether or not the character is crouching in the animator
 		anim.SetBool("Crouch", crouch);
-
+		
 		//only control the player if grounded or airControl is turned on
 		if(grounded || airControl)
 		{
 			// Reduce the speed if crouching by the crouchSpeed multiplier
 			move = (crouch ? move * crouchSpeed : move);
-
+			
 			// The Speed animator parameter is set to the absolute value of the horizontal input.
 			anim.SetFloat("Speed", Mathf.Abs(move));
-
+			
 			// Move the character
 			rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
 			
@@ -77,15 +87,15 @@ public class PlatformerCharacter2D : MonoBehaviour
 				// ... flip the player.
 				Flip();
 		}
-
-        // If the player should jump...
-        if (grounded && jump) {
-            // Add a vertical force to the player.
-            anim.SetBool("Ground", false);
-            rigidbody2D.AddForce(new Vector2(0f, jumpForce));
-        }
+		
+		// If the player should jump...
+		if (grounded && jump) {
+			// Add a vertical force to the player.
+			anim.SetBool("Ground", false);
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+		}
 	}
-
+	
 	
 	void Flip ()
 	{
---
+++ Assets/scripts/JetPack.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JetPack : MonoBehaviour {
+
+	private float gravity = 20.0f;
+	private Vector3 moveDirection = Vector3.zero;
+	private SpriteRenderer spriteRenderer;
+	public Sprite jetPackSprite;
+
+	public bool inInventory = false;
+
+	Collision2D coll;
+
+	void Start(){
+		if (inInventory) {
+
+			GameObject.Destroy (gameObject);
+		}
+	}
+
+	public PlatformerCharacter2D character;
+	// Use this for initialization
+	void OnCollisionEnter2D(Collision2D coll) {
+
+				if (coll.gameObject.tag == "Player") {
+
+						inInventory = true;
+						GameObject.Destroy (gameObject);
+	
+						character.rigidbody2D.gravityScale = 1;
+						moveDirection.y -= gravity * Time.deltaTime;
+						character.airControl = true;
+			
+						spriteRenderer = character.GetComponent<SpriteRenderer> ();
+						spriteRenderer.sprite = jetPackSprite;
+
+						character.hasJetPack = true;
+				}
+	}
+
+}
