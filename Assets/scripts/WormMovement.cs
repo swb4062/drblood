713c2288ce3460c6489daf1e7ecb319c6449c001
Levi Flippo <levi.flippo@toastmobile.com>
2014-12-01 13:32:52 -0600

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Library/ScriptAssemblies/Assembly-CSharp-Editor.dll
    	Library/ScriptAssemblies/Assembly-CSharp-Editor.dll.mdb
    	Library/ScriptAssemblies/Assembly-CSharp.dll
    	Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    	Library/assetDatabase3
    	Library/guidmapper
    	Library/metadata/36/3689e113a07804143a3f9c0baaee8cc8
    	Library/metadata/ba/bac1cf0122ad7aa4c95b039681462604
    	drblood-csharp.sln
    	drblood.sln
    
    start of worm movement

---
+++ Assets/scripts/WormMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WormMovement : MonoBehaviour {
+
+	public Vector3 speed = new Vector3(0,5.0F,0);
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (renderer.isVisible) {
+		} else {
+			//speed = -speed;
+			transform.Rotate(0,0, 180);
+		}
+		transform.Translate(speed * Time.deltaTime);
+	}
+}
